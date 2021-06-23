cc7475b48a714578a13c56c88e7c23171ac48097
irion-silva <irion.silva@hotmail.com>
2021-06-23 17:56:20 -0300

    Novo commit

--- pacote-download/Calculadora/src/calculadora/Calculadora.java
+++ pacote-download/Calculadora/src/calculadora/Calculadora.java
@@ -19,46 +19,58 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("BEM VINDO A CALCULADORA!");
-        System.out.print("Digite o primeiro valor:");
         int num1, num2, resultado;
-        num1 = teclado.nextInt();
-        System.out.print("Digite o segundo valor:");
-        num2 = teclado.nextInt();
-        System.out.println("Digite soma, para somar, subtracao para subtrair, divisao para dividir, multiplicacao, para multiplicar e mod para tirar o resto: ");
-        String caso = teclado.next();
-        resultado = 0;
-        switch (caso){
-            case "soma":
-                resultado = num1+num2;
-                System.out.println("A soma é: "+ resultado);
-                break;
-                
-            case "subtracao":
-                resultado = num1 - num2;
-                System.out.println("A subtracao é " + resultado );
-            break;
+        String CharOpc;
+        boolean encerrar = false;
+        do {
+            System.out.print("Digite o primeiro valor:");
+            num1 = teclado.nextInt();
+            System.out.print("Digite o segundo valor:");
+            num2 = teclado.nextInt();
+            System.out.println("Digite soma, para somar, subtracao para subtrair, divisao para dividir, multiplicacao, para multiplicar e mod para tirar o resto: ");
+            String caso = teclado.next();
+            resultado = 0;
+            switch (caso) {
+                case "soma":
+                    resultado = num1 + num2;
+                    System.out.println("A soma é: " + resultado);
+                    break;
 
-            case "divisao":
-                resultado = num1 / num2;
-                System.out.println("A divisao é " + resultado );
-            break;
+                case "subtracao":
+                    resultado = num1 - num2;
+                    System.out.println("A subtracao é " + resultado);
+                    break;
 
-            case "multiplicacao":
-                resultado = num1 * num2;
-                System.out.println("A multiplicação é " + resultado );
-            break;
+                case "divisao":
+                    resultado = num1 / num2;
+                    System.out.println("A divisao é " + resultado);
+                    break;
 
-            case "mod":
-                resultado = num1 % num2;
-                System.out.println("O resto é " + resultado );
-            break;
+                case "multiplicacao":
+                    resultado = num1 * num2;
+                    System.out.println("A multiplicação é " + resultado);
+                    break;
 
-            default:
-                System.out.println("Digite algo válido");
-            
-        }
+                case "mod":
+                    resultado = num1 % num2;
+                    System.out.println("O resto é " + resultado);
+                    break;
+
+                default:
+                    System.out.println("Digite algo válido");
+
+            }
+            System.out.print("Deseja continuar[S/N]:");
+            CharOpc = teclado.next();
+            if (CharOpc.equals("N") || CharOpc.equals("n")){
+                encerrar = true;
+            }
+        } while (encerrar == false);
         
 
+    
+    
+    
     }
 
 }
